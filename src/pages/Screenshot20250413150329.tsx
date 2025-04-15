6647d19f513c38270c4a05d949fa10d860477c33
Samcreatesthat <Samcreatesthat>
2025-04-15 05:11:57 +0000

    Pushed from Locofy Builder

--- src/pages/Screenshot20250413150329.tsx
+++ src/pages/Screenshot20250413150329.tsx
@@ -1,7 +1,10 @@
-const Screenshot20250413150329 = () => {
+import { FunctionComponent } from 'react';
+
+
+
+const Screenshot20250413150329:FunctionComponent = () => {
   return (
-    <div className="w-full relative h-[1058px] bg-[url('/public/screenshot-20250413-150329-1@3x.png')] bg-cover bg-no-repeat bg-[top]" />
-  );
+    <div className="w-full relative h-[1058px] bg-[url('/public/screenshot-20250413-150329-1@3x.png')] bg-cover bg-no-repeat bg-[top]" />);
 };
 
 export default Screenshot20250413150329;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    fontSize: {
-      inherit: "inherit",
-    },
+    module.exports = {
+  "content": [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  "theme": {
+    "fontSize": {
+      "inherit": "inherit"
+    }
   },
-  corePlugins: {
-    preflight: false,
-  },
-};
+  "corePlugins": {
+    "preflight": false
+  }
+}
